b716915570c8cfa6a3b3c13fe6f286c54039a212
youth lee <piaoliuwuyan@163.com>
2019-11-23 15:05:48 +0800

    config dialect

--- src/main/java/org/crud2/spring/boot/autoconfigure/Crud2AutoConfiguration.java
+++ src/main/java/org/crud2/spring/boot/autoconfigure/Crud2AutoConfiguration.java
@@ -4,6 +4,7 @@ import org.crud2.CRUD;
 import org.crud2.CRUD2BeanFactory;
 import org.crud2.Initializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -25,10 +26,17 @@ public class Crud2AutoConfiguration {
     @Autowired
     private Initializer initializer;
 
+    @Value("${crud2.dialect}")
+    private String dialect;
+
     @PostConstruct
     public void init() {
         crud.setBeanFactory(crud2BeanFactory);
+        if (dialect != null && dialect.length() > 0) {
+            CRUD.dialect = dialect;
+        }
         initializer.initialize();
     }
+
 
 }
